19fa5a1f89d7be6ce92b451ac97697186fa9d7d7
omerzomam19 <zomamomer19@gmail.com>
2025-07-24 10:23:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -295,4 +295,4 @@ def delete_cv(cv_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()
-     app.run(host="0.0.0.0", port='5000') 
+ app.run(host="0.0.0.0", port=5000)
